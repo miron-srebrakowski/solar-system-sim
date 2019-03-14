8d1317276b13b6a1da8b26bed8f31871f25bc2ee
mironski <47178615+mironski@users.noreply.github.com>
2019-03-14 13:36:28 +0000

    Update project.py

--- project.py
+++ project.py
@@ -6,54 +6,43 @@ import matplotlib.pyplot as pyplot
 import functions as f
 import integration
 
-
 """
-For period calculation takes start point and calculate time each objects
-travels 360 around the sun.
+Program containing the main function of the solar system simulation.
 """
-
-
 
 def main():
 
-    #Takes 3 file names from comand line and opens them for use. 1) File with initial conditions of objects (read);
+    #Takes 3 file handle arguments and opens them 
 
-    #2) Trajectory output file (write); 3) System energy output file (write).
-
-
-    if len(sys.argv)!=6:
+    if len(sys.argv)!=4:
         print("Wrong number of arguments.")
-        print("Usage: " + sys.argv[0] + "<intial cond file>" + "<trajectory file>" + "<parameter file>" + "<energy file>" + "<observables file>")
+        print("Usage: " + sys.argv[0] + "<intial cond file>" + "<trajectory file>" + "<parameter file>" )
         quit()
     else:
-        init_name = sys.argv[1]
-        traj_name = sys.argv[2]
-        param_name = sys.argv[3]
-        energy_name = sys.argv[4]
-        observables_name = sys.argv[5]
+        infile = open(sys.argv[1], "r") #file containing the initial conditions of the objects
+        traj_file = open(sys.argv[2], "w") #output trajectory file
+        param_file = open(sys.argv[3], "r") #file containing the simulation parameters (number of objects, timestep size and number of time steps)
+        energy_file = open("energy.dat", "w") #output total system energy file
+        observables_file = open("observables.dat", "w") #output file to contain physical observables of the system
 
 
-    infile = open(init_name, "r")
-    traj_file = open(traj_name, "w")
-    param_file = open(param_name, "r")
-    energy_file = open(energy_name, "w")
-    observables_file = open(observables_name, "w")
+    #Initialises the time step, number of steps and number of objects from
+    #a parameter file.
 
-
-    #Time step in days.
     line = param_file.readline()
     linesplit = line.split(" ")
     object_number = int(linesplit[0])
     dt = float(linesplit[1])
     steps = int(linesplit[2])
 
+    #Initialises lists used in the simulation.
 
     energy_list = [] #contains the system energies at each time step.
     time_list = [] #contains the time steps.
     object_list = [] #contains the objects.
-    prev_pos_list = []
-    angle_list = []
-    period_list = []
+    prev_pos_list = [] #contains previous object positions
+    angle_list = [] #contains total angular distance travelled by each object
+    period_list = [] #contains the orbital periods of each object
 
 
     force = [] #contains new forces. [nx3]
@@ -87,28 +76,22 @@ def main():
         for l in range(0,len(force)):
             force_prev[l]=force[l]
 
-
-
 # Corrects for centre of mass.
 
     f.com_corr(object_list)
 
-
-# Main simulation loop:
-#1) Prints pos & v to traj file
-#2) Updates pos using force (for first time step force = initial force)
-#3) Calculates the sum of forces on each object
-#4) Stores each force sum in force.
-#5) Updates v of each objects with force and previous force of each object
-#6) Sets force to previous force
+# Main time integration loop. Fills trajectory file, finds total angular distance travelled byeach body
+# and calculates total energy of the system at each time step.
 
     integration.time_integration (steps, object_list, traj_file, object_number, prev_pos_list, dt, force, min_list, max_list, angle_list, force_sum, force_prev, energy_file, energy_list, time_list)
 
-    print(angle_list[1])
+# Calculates the orbital period of each object and stores it in a list
+
     for i in range(len(angle_list)):
         period_list.append(steps / (np.rad2deg(angle_list[i]) / 360))
 
 
+# Writes the values of the physical observables of each object into a file
 
     for i in range (0, len(out_list)):
         observables_file.write(str(out_list[i]) + "\n")
@@ -117,8 +100,7 @@ def main():
         observables_file.write("Orbital Period: " + str(period_list[i]) + "\n")
         observables_file.write("\n")
 
-    # Plot system energy to screen
-
+    # Plots system energy to screen
 
     pyplot.title('Solar system: total energy vs time')
     pyplot.xlabel('Time')
@@ -126,5 +108,14 @@ def main():
     pyplot.plot(time_list, energy_list)
     pyplot.show()
 
+# Closes input/output files
+
+    infile.close()
+    traj_file.close()
+    param_file.close()
+    energy_file.close()
+    observables_file.close()
+
+# Runs main function
 
 main()
